0bd9c9f6f9b43215e510256401c5ab4c9bf4817a
jluisferrer <jluisferrermartinez@gmail.com>
2024-04-29 20:59:05 +0200

    feat:groupSeeder working

--- database/seeders/GroupSeeder.php
+++ database/seeders/GroupSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
 
 class GroupSeeder extends Seeder
 {
@@ -14,6 +16,15 @@ class GroupSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 20; $i++) {
+            DB::table('group')->insert([
+                'groupName' => $faker->name,               
+                'groupImage' => $faker->imageUrl(640, 480, 'events'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
